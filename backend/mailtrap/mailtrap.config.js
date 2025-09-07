8b017a68dba5b07dbd16d85dcf22c389916deca0
Roshaid Atique <roshaid2004@gmail.com>
2025-09-07 17:33:30 +0530

    project completion

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import { MailtrapClient } from "mailtrap";
+
+dotenv.config();
+
+const TOKEN = process.env.MAILTRAP_TOKEN;
+const ENDPOINT = process.env.MAILTRAP_ENDPOINT;
+
+export const mailtrapClient = new MailtrapClient({
+    endpoint: ENDPOINT,
+    token: TOKEN,
+});
+
+export const sender = {
+    email: "hello@demomailtrap.co",
+    name: "Roshaid",
+};
